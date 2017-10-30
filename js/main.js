7e8ddac3d90ba5a2cc32aa1d6100b30539d597ca
Michael Sanchez <michajsa@gene.com>
2017-10-30 14:30:30 -0700

    week8exercise

---
+++ js/main.js
@@ -0,0 +1,32 @@
+for(var i = 0; i <= 100; i += 5) {
+  console.log(i);
+}
+
+function getValue() {
+  var input = document.getElementById('input');
+  console.log(input.value);
+}
+
+// numbers, strings, arrays, booleans, objects
+
+var batman = {
+  color: 'black',
+  alliance: 'good',
+  power: 'rich AF',
+};
+
+var spiderman = {
+  color:'Blue and red',
+  alliance: 'good',
+  power: 'web Slinger'
+};
+
+var joker = {
+  color: 'purple',
+  alliance: 'Bad',
+  power: 'Crazy AF'
+};
+
+console.log(batman);
+// console.log(spiderman);
+// console.log(joker);
